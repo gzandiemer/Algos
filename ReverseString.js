71f7032f7510def9dfe0def014cd30c26e658ffc
Günışığı Zan Diemer <43114601+gzandiemer@users.noreply.github.com>
2020-01-07 10:03:50 +0100

    Create ReverseString.js

---
+++ ReverseString.js
@@ -0,0 +1,9 @@
+function reverse(str) {
+
+   // 1. Turn string into an array
+   // 2. Reverse the elements within the array
+   // 3. Turn the array back to a string
+   
+   return str.split('').reverse().join('');
+
+}
