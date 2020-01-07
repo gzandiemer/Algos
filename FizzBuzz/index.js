2eae32d88856829cc82b539ac9cd09bfe6c9db65
Günışığı Zan Diemer <43114601+gzandiemer@users.noreply.github.com>
2020-01-07 11:04:24 +0100

    Create index.js

---
+++ FizzBuzz/index.js
@@ -0,0 +1,22 @@
+function fizzBuzz(n) {
+  const i = 1;
+  
+  while(i <= n) {
+    if (i%3 == 0 && i%5 == 0) {
+      console.log('FizzBuzz');
+    } else if (i%3 == 0){
+      console.log('Fizz');
+    } else if (i%5 == 0){
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
+    
+    i++;
+  }
+
+}
+
+
+
+module.exports = fizzBuzz;
