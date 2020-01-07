02c61dba47b694f5f8bf0ca5875f292d897eb54d
Günışığı Zan Diemer <43114601+gzandiemer@users.noreply.github.com>
2020-01-07 10:58:57 +0100

    Update index.js

--- ReverseString/index.js
+++ ReverseString/index.js
@@ -2,8 +2,10 @@ function reverse(str) {
 
    // 1. Turn string into an array
    // 2. Reverse the elements within the array
-   // 3. Turn the array back to a string
+   // 3. Turn the array back into a string
    
    return str.split('').reverse().join('');
 
 }
+
+module.exports = reverse;
